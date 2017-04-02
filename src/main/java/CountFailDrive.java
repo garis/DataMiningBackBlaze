0949af287a17ad805f71a0e5cbefad7a4789f1fa
riccardo <garis94@gmail.com>
2017-04-02 20:00:42 +0200

    -code for counting disks failures now revised and under testing Now is really done, not like I want but is fast and doesn't use 10GB+ of memory

--- src/main/java/CountFailDrive.java
+++ src/main/java/CountFailDrive.java
@@ -1,4 +1,4 @@
-/*UNDER TESTING
+/*
 final String data_path = Utils.path;
         System.out.println("Data path: " + data_path);
 
@@ -7,88 +7,46 @@ final String data_path = Utils.path;
                 .setMaster("local")
         );
 
-        JavaPairRDD<String,String> textFile = spark_context.wholeTextFiles(data_path + "Out2_HardDisksFilesOrdered", 1000);
+        JavaPairRDD<String, String> textFile = spark_context.wholeTextFiles(data_path + "Data", 1000);
 
-        JavaPairRDD<String, String[]> rows = textFile.mapToPair(file ->
+        JavaPairRDD<String, Integer> rows = textFile.mapToPair(file ->
         {
             String[] righe = file._2().split(String.format("%n"));
-            //righe.length-3 optimize search ;) (prerequisite=ordered records) I NEED TO THINK ABOUT THIS
-            for(int i=0;i<righe.length;i++)
-            {
+            int contatore = 0;
+            String data = "";
+            //start from the second line
+            for (int i = 1; i < righe.length; i++) {
                 String[] valori = righe[i].split(",");
-                if(valori[3].compareTo("1")==0)
-                {
-                    //WTF MOMENT.....
-                    String[] copia=new String[valori.length];//resolve:java.lang.String cannot be cast to [Ljava.lang.String
-                    for(int j=0;j<valori.length;j++)
-                    {
-                        copia[j]=valori[j];
-                    }
-                    //END OF WTF MOMENT
-                                //the SN
-                    return new Tuple2(valori[1], copia);
+
+                data = valori[0];
+                if (valori[4].compareTo("1") == 0) {
+                    contatore++;
                 }
             }
-            String[] empty=new String[1];empty[0]="0";
-            return new Tuple2("0", empty);
+            return new Tuple2(data, contatore);
         });
 
-        //for debug purpouses
-        //rows.foreach((Tuple2<String,String[]> disco)->
-        //{
-        //  System.out.println(disco._1());
-        //  for(int i=0;i<disco._2().length;i++)
-        //  {
-        //      System.out.print(disco._2()[i]);
-        //  }
-        //  System.out.println("\n");
-        //});
+        java.util.Map<String, Integer> result = rows.collectAsMap();
 
         String filename = data_path + "failureStat.csv";
-                File file = new File(filename);
-                if (!file.exists()) {
-                try {
+        File file = new File(filename);
+        if (!file.exists()) {
+            try {
                 file.createNewFile();
-                } catch (IOException e) {
+            } catch (IOException e) {
                 e.printStackTrace();
-                }
-                }
-                FileWriter fw = null; // creating fileWriter object with the file
-                try {
-                fw = new FileWriter(file.getAbsoluteFile(), false);
-                } catch (IOException e) {
+            }
+        }
+        FileWriter fw = null;
+        fw = new FileWriter(file.getAbsoluteFile(), false);
+        BufferedWriter bw = new BufferedWriter(fw);
+
+        result.forEach((String key, Integer value) -> {
+            try {
+                bw.write(key + "," + value + String.format("%n"));
+            } catch (IOException e) {
                 e.printStackTrace();
-                }
-                BufferedWriter bw = new BufferedWriter(fw); // creating bufferWriter which is used to write the content into the file
-                String value = "";
-
-                int anno = 2016;
-                for (int mese = 1; mese < 13; mese++) {
-        for (int giorno = 1; giorno < 32; giorno++) {
-final String data = anno + "-" +String.format("%02d", mese) + "-" + String.format("%02d", giorno);
-        java.util.Map<String,Long> number = rows.filter((Tuple2<String, String[]> riga) ->
-        {
-        if (riga._2()[0].compareTo(data) == 0)
-        return true;
-        return false;
-        }).countByKey();
-
-        String line = data + ","+number.size()+"," +String.format("%n", "");
-        try {
-        bw.write(line);
-        } catch (IOException e) {
-        e.printStackTrace();
-        }
-        try {
-        bw.flush();
-        } catch (IOException e) {
-        e.printStackTrace();
-        }
-        }
-        }
-        try {
+            }
+        });
         bw.close();
-        } catch (IOException e) {
-        e.printStackTrace();
-        }
  */
