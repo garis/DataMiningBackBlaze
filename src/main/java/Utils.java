f0d71d17a1008858488da57b37dc2abc01e2138c
Pastea <andrea.mattiazzo.2@studenti.unipd.it>
2017-05-12 11:39:15 +0200

    Tried to ignore Utils.java

--- src/main/java/Utils.java
+++ src/main/java/Utils.java
@@ -1,3 +1,3 @@
 public class Utils {
-    public static String path="/home/riccardo/Universita/DataMining/backblazeHDDstats/";
+    public static String path="D:/UNIPD/DataMining/Progetto/backblazeHDDstats/";
 }
